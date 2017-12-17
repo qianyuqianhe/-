311f9b2e82a0c0a7b7bd9a0667931661989be55f
qianyuqianhe <chengxuan26@gmail.com>
2017-12-17 22:10:20 +0800

    20171217-2210

--- 织梦仿站助手/Form1.Designer.cs
+++ 织梦仿站助手/Form1.Designer.cs
@@ -33,19 +33,30 @@
             this.btnOk = new System.Windows.Forms.Button();
             this.txtLog = new System.Windows.Forms.RichTextBox();
             this.btnClear = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.about = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // txtUrl
             // 
-            this.txtUrl.Location = new System.Drawing.Point(59, 11);
+            this.txtUrl.Location = new System.Drawing.Point(163, 104);
             this.txtUrl.Name = "txtUrl";
-            this.txtUrl.Size = new System.Drawing.Size(298, 21);
+            this.txtUrl.Size = new System.Drawing.Size(416, 21);
             this.txtUrl.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Location = new System.Drawing.Point(116, 107);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(41, 12);
             this.label1.TabIndex = 1;
@@ -53,7 +64,7 @@
             // 
             // btnOk
             // 
-            this.btnOk.Location = new System.Drawing.Point(375, 9);
+            this.btnOk.Location = new System.Drawing.Point(603, 102);
             this.btnOk.Name = "btnOk";
             this.btnOk.Size = new System.Drawing.Size(75, 23);
             this.btnOk.TabIndex = 2;
@@ -65,7 +76,7 @@
             // 
             this.txtLog.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.txtLog.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.txtLog.Location = new System.Drawing.Point(0, 133);
+            this.txtLog.Location = new System.Drawing.Point(0, 472);
             this.txtLog.Name = "txtLog";
             this.txtLog.Size = new System.Drawing.Size(948, 128);
             this.txtLog.TabIndex = 3;
@@ -73,7 +84,7 @@
             // 
             // btnClear
             // 
-            this.btnClear.Location = new System.Drawing.Point(861, 104);
+            this.btnClear.Location = new System.Drawing.Point(859, 281);
             this.btnClear.Name = "btnClear";
             this.btnClear.Size = new System.Drawing.Size(75, 23);
             this.btnClear.TabIndex = 4;
@@ -81,21 +92,114 @@
             this.btnClear.UseVisualStyleBackColor = true;
             this.btnClear.Click += new System.EventHandler(this.button1_Click);
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(948, 336);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.txtUrl);
+            this.tabPage1.Controls.Add(this.btnClear);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.btnOk);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(940, 310);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "提取资源";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.about);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(940, 310);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "关于";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(367, 159);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(173, 21);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "QQ群：673538184";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(367, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(163, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "QQ：2353631858";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(214, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(538, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "软件使用过程中有任何意见或建议请与我们联系！";
+            // 
+            // about
+            // 
+            this.about.AutoSize = true;
+            this.about.Font = new System.Drawing.Font("宋体", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.about.Location = new System.Drawing.Point(306, 235);
+            this.about.Name = "about";
+            this.about.Size = new System.Drawing.Size(303, 33);
+            this.about.TabIndex = 0;
+            this.about.Text = "冥灵工作室版权所有";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 344);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScrollBarsEnabled = false;
+            this.webBrowser1.Size = new System.Drawing.Size(948, 128);
+            this.webBrowser1.TabIndex = 6;
+            this.webBrowser1.Url = new System.Uri("http://minglingstudio.com/ad/zmzs728-90.html", System.UriKind.Absolute);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(948, 261);
-            this.Controls.Add(this.btnClear);
+            this.ClientSize = new System.Drawing.Size(948, 600);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.txtLog);
-            this.Controls.Add(this.btnOk);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtUrl);
             this.Name = "Form1";
             this.Text = "织梦仿站助手";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -106,6 +210,14 @@
         private System.Windows.Forms.Button btnOk;
         private System.Windows.Forms.RichTextBox txtLog;
         private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label about;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- 织梦仿站助手/Form1.cs
+++ 织梦仿站助手/Form1.cs
@@ -35,12 +35,21 @@ namespace 织梦仿站助手
         }
         List<string> cssFilePathList = new List<string>();
         List<string> cssRelativeUrls = new List<string>();
+        List<string> cssFileName = new List<string>();
+
         List<string> jsFilePathList = new List<string>();
         List<string> jsRelativeUrls = new List<string>();
+        List<string> jsFileName = new List<string>();
+
         List<string> imgFilePathList = new List<string>();
         List<string> imgRelativeUrls = new List<string>();
+        List<string> imgFileName = new List<string>();
+
         List<string> urlFilePathList = new List<string>();
         List<string> urlRelativeUrls = new List<string>();
+
+        string text="";
+        string url="";
         /// <summary>
         /// 提取按钮的单击事件
         /// </summary>
@@ -65,15 +74,16 @@ namespace 织梦仿站助手
                 Directory.CreateDirectory("url");
             }
 
-            string url = txtUrl.Text;
-            string text = GetHttpWebRequest(url);
+            url = txtUrl.Text;
+            text = GetHttpWebRequest(url);
 
 
             //提取css文件
             string[] regularCss = {
                 "<link.*rel=\"Stylesheet\".*href=\"(.*?)\"",
                 "<link.*rel=\"stylesheet\".*href=\"(.*?)\"",
-                "<link href='(.*?)' rel='stylesheet'"
+                "<link href='(.*?)' rel='stylesheet'",
+                "<link href=\"(.*?)\".*rel=\"stylesheet\">"
             };            
             foreach(string regular in regularCss)
             {
@@ -135,7 +145,7 @@ namespace 织梦仿站助手
                 }
                 
             }
-            //开启线程下载文件
+            //开启线程下载文件并生成预处理的网页文件
             Thread DownloadFile = new Thread(new ParameterizedThreadStart(ThreadDownload));
             DownloadFile.Start();
         }
@@ -146,20 +156,32 @@ namespace 织梦仿站助手
         /// <returns>页面源码</returns>
         private string GetHttpWebRequest(string url)
         {
-            Uri uri = new Uri(url);
-            HttpWebRequest myReq = (HttpWebRequest)WebRequest.Create(uri);
-            myReq.UserAgent = "User-Agent:Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705";
-            myReq.Accept = "*/*";
-            myReq.KeepAlive = true;
-            myReq.Headers.Add("Accept-Language", "zh-cn,en-us;q=0.5");
-            HttpWebResponse result = (HttpWebResponse)myReq.GetResponse();
-            Stream receviceStream = result.GetResponseStream();
-            StreamReader readerOfStream = new StreamReader(receviceStream, System.Text.Encoding.GetEncoding("utf-8"));
-            string strHTML = readerOfStream.ReadToEnd();
-            readerOfStream.Close();
-            receviceStream.Close();
-            result.Close();
-            return strHTML;
+            try {
+                Uri uri = new Uri(url);
+                HttpWebRequest myReq = (HttpWebRequest)WebRequest.Create(uri);
+                myReq.Method = "GET";
+                myReq.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36";                    
+                myReq.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8";
+                myReq.KeepAlive = true;
+                myReq.Headers.Add("Accept-Language", "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7");
+                myReq.Referer = url;
+                myReq.AllowAutoRedirect = false;
+                HttpWebResponse result = (HttpWebResponse)myReq.GetResponse();
+                Stream receviceStream = result.GetResponseStream();
+                StreamReader readerOfStream = new StreamReader(receviceStream, System.Text.Encoding.GetEncoding("utf-8"));
+                string strHTML = readerOfStream.ReadToEnd();
+                readerOfStream.Close();
+                receviceStream.Close();
+                result.Close();
+                return strHTML;
+            }
+            catch(Exception e)
+            {
+
+                updateRunLog(String.Format("获取页面源码出错，发生在链接：{0}\r\n",url));
+                updateRunLog(e.ToString()+"\r\n");
+                return "";
+            }
         }
         /// <summary>
         /// 通过正则表达式匹配内容
@@ -170,10 +192,16 @@ namespace 织梦仿站助手
         private List<string> GetRegularExpressionValue(string text, string regular)
         {
             List<string> value = new List<string>();
-            MatchCollection _matchCollection = Regex.Matches(text, regular);
-            foreach (Match match in _matchCollection)
+            try { 
+                MatchCollection _matchCollection = Regex.Matches(text, regular);
+                foreach (Match match in _matchCollection)
+                {
+                    value.Add(match.Groups[1].Value.ToString());
+                }
+            }
+            catch
             {
-                value.Add(match.Groups[1].Value.ToString());
+                value = new List<string>();
             }
             return value;
         }
@@ -208,7 +236,18 @@ namespace 织梦仿站助手
                 int i = url.LastIndexOf("/");
                 filename=url.Substring(i);
             }
-            
+            if (path.Equals("style"))
+            {
+                cssFileName.Add("{dede:global.cfg_templets_skin/}/style/"+filename);
+            }
+            if (path.Equals("js"))
+            {
+                jsFileName.Add("{dede:global.cfg_templets_skin/}/js/" + filename);
+            }
+            if (path.Equals("images"))
+            {
+                imgFileName.Add("{dede:global.cfg_templets_skin/}/images/" + filename);
+            }
             path += "/" + filename;
             if (!File.Exists(path))
             {
@@ -265,18 +304,48 @@ namespace 织梦仿站助手
                 HttpDownload(url, "url");
             }
             updateRunLog(String.Format("下载url文件完成，共{0}个文件。\r\n", urlFilePathList.Count));
+            //生成预处理的网页文件
+            string newhtml = text;
+            for (int i = 0; i < cssFileName.Count; i++)
+            {
+                newhtml = newhtml.Replace(cssRelativeUrls[i], cssFileName[i]);
+            }
+            for (int i = 0; i < jsFileName.Count; i++)
+            {
+                newhtml = newhtml.Replace(jsRelativeUrls[i], jsFileName[i]);
+            }
+            for (int i = 0; i < imgFileName.Count; i++)
+            {
+                newhtml = newhtml.Replace(imgRelativeUrls[i], imgFileName[i]);
+            }
+            File.WriteAllText("index.html", newhtml, Encoding.UTF8);
+            updateRunLog("生成预处理的网页文件index.html成功!\r\n");
         }
         private void button1_Click(object sender, EventArgs e)
         {
             txtLog.Text = "";
             cssFilePathList = new List<string>();
             cssRelativeUrls = new List<string>();
+            cssFileName = new List<string>();
+
             jsFilePathList = new List<string>();
             jsRelativeUrls = new List<string>();
+            jsFileName = new List<string>();
+
             imgFilePathList = new List<string>();
             imgRelativeUrls = new List<string>();
+            imgFileName = new List<string>();
+
             urlFilePathList = new List<string>();
             urlRelativeUrls = new List<string>();
+
+            text = "";
+            url = "";
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show(GetHttpWebRequest("http://www.mcsite.cn/"));
         }
     }
 }
